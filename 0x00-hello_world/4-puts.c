79d6e6d67eb6ae73878b983248593e51ef811efd
“Auggie90” <auggieobimma@gmail.com>
2022-04-07 14:19:07 -0700

    ‘message’

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- * main - Entry point                                                                                                                   
- *                                                                                                                                      
- * Return: 0 printing the function                                                                                                      
- */                                                                                                                                     
-int main(void) {puts("\"Programming is like building a multilingual puzzle"); return (0); }
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 printing the function
+ */
+int main(void)
+{puts("\"Programming is like a multilingual puzzle");return (0);}

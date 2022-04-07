3a801eaaa42b1f4b367af93026b10af6cfbcfd17
“Auggie90” <auggieobimma@gmail.com>
2022-04-07 14:06:02 -0700

    ‘message’

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
 #include <stdio.h>                                                                                                                      
 /**                                                                                                                                     
- *  * main - Entry point                                                                                                                   
- *   *                                                                                                                                      
- *    * Return: 0 printing the function                                                                                                      
- *     */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-	puts("\"Programming is like building a multilingual puzzle");                                                                           
-	return (0);                                                                                                                             
-}
+ * main - Entry point                                                                                                                   
+ *                                                                                                                                      
+ * Return: 0 printing the function                                                                                                      
+ */                                                                                                                                     
+int main(void) {puts("\"Programming is like building a multilingual puzzle"); return (0); }

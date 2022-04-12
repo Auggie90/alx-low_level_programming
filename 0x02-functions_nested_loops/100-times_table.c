515caa7bdd881a310a0d341963c5e95f4204b9ca
“Auggie90” <auggieobimma@gmail.com>
2022-04-12 15:58:47 -0700

    ‘message’

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,18 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_times_table - prints n timestable
+ * main - check the code.
  *
- * @n: parameter
- * Return: 0
+ * Return: Always 0.
  */
-
-void print_times_table(int n)
+int main(void)
 {
-int i, j, mult;
-
-if (n >= 0 && n <= 15)
-{
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++){
-mult = i * j;
-
-if (mult < 10)
-{
-if (j >= 1)
-{
-_putchar(' ');
-_putchar((mult / 10) + ' ');
-}
-_putchar((mult % 10) + '0');
-}
-else if (mult < 100)
-{
-_putchar(' ');
-_putchar((mult / 10) + '0');
-_putchar((mult % 10) + '0');
-}
-else
-{
-_putchar((mult / 100) + '0');
-_putchar(((mult - 100) / 10) + '0');
-_putchar((mult % 10) + '0');
-}
-if (j != n)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
+print_times_table(3);
 _putchar('\n');
-}
-}
+print_times_table(5);
+_putchar('\n');
+print_times_table(98);
+_putchar('\n');
+print_times_table(12);  
+return (0);
 }

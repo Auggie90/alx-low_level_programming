a91dd24e312d996e3d0cfbd6087400e820097da3
“Auggie90” <auggieobimma@gmail.com>
2022-04-13 06:02:52 -0700

    ‘message’

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * print_times_table - prints times table
  * @n : times table to use
@@ -10,23 +8,30 @@
 
 void print_times_table(int n)
 {
+
 int a = 0, rep, b;
 
 if (n < 0 || n > 15)
 return;
+
 while (a <= n)
 {
+
 for (b = 0; b <= n; b++)
 {
+
 rep = a * b;									if (b == 0)
 _putchar('0' + rep);								else if (rep < 10)
 {
+
 _putchar(' ');									_putchar(' ');
 _putchar('0' + rep);								}										else if (rep < 100)
 {										_putchar(' ');									_putchar('0' + rep / 10);
 _putchar('0' + rep % 10);
+
 }
 else										{
+
 _putchar('0' + rep / 100);
 _putchar('0' + (rep - 100) / 10);						_putchar('0' + rep % 10);
 										}

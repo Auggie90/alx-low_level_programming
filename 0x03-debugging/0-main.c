fa3991e935a29465a43a161b29f9e3ba330c7cb1
“Auggie90” <auggieobimma@gmail.com>
2022-04-13 13:14:55 -0700

    ‘message’

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,9 @@
 #include "main.h"
 
-
-
 /**
  * main - Test function for positive or negative
  * Return: 0
  */
-
-
 
 int main(void)
 
@@ -15,13 +11,9 @@ int main(void)
 
 int i;
 
-
-
 i = 0;
 
 positive_or_negative(i);
-
-
 
 return (0);
 

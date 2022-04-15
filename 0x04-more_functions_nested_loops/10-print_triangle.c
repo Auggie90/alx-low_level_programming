894a912f34510a5cf317e7fa3cf08f75542beac2
“Auggie90” <auggieobimma@gmail.com>
2022-04-14 17:15:51 -0700

    ‘message’

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,11 +6,8 @@
  */
 
 void print_triangle(int size)
-
 {
-
 if (size <= 0)
-
 {
 _putchar('\n');
 } 
@@ -21,11 +18,8 @@ for (i = 1; i <= size; i++)
 {
 for (j = i; j < size; j++)
 {
-
 _putchar(' ');
-
 }
-
 for (j = 1; j <= i; j++)
 {
 _putchar('#');									}

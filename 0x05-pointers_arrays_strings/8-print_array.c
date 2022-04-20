37bc2eb2f1e41c10ca678b92a0625e4174c19527
“Auggie90” <auggieobimma@gmail.com>
2022-04-20 14:31:44 -0700

    ‘message’

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,22 +8,17 @@
  */
 
 void print_array(int *a, int n)
-
 {
 int i;
-
 i = 0;
 while (i < n)
 {
 printf("%d", a[i]);
 
 if (i < n - 1)
-														{
-														printf(", ");
-														}
-
-i++;
-														}
+{														printf(", ");
+}
+i++;														}
 
 printf("\n");
 }

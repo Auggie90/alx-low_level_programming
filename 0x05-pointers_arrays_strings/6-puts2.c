093d841b5c07dc601d77a95316a676c7d42c90a1
“Auggie90” <auggieobimma@gmail.com>
2022-04-20 14:04:33 -0700

    ‘message’

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,13 @@
 
 void puts2(char *str)
 {
-for (; *str != '\0'; str += 2)
+int a;
+
+for (a = 0; str[a] != '\0'; a++)
 {
-_putchar(*str);
+if (a % 2 == 0)
+_putchar (str[a]);
 }
-_putchar('\n');
+
+_putchar ('\n');
 }

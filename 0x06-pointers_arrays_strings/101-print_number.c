a5bccb46c13ecca8aaae28563099ec19fdd2a7ef
“Auggie90” <auggieobimma@gmail.com>
2022-04-25 06:56:25 -0700

    ‘message’

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,35 +1,28 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ * print_number - prints number
+ *
+ * @n: number param
  */
+
+
 void print_number(int n)
 {
-unsigned int m, d, count;
-
+unsigned int num = 0;
 if (n < 0)
 {
-_putchar(45);
-										m = n * -1;
-										}
+num = -n;
+_putchar('-');
+}
 else
 {
-m = n;
-										}
+num = n;
+}
 
-d = m;
-count = 1;
-
-while (d > 9)
+if (num / 10)
 {
-										d /= 10;
-										count *= 10;
-										}
-
-for (; count >= 1; count /= 10)
-										{
-										_putchar(((m / count) % 10) + 48);
-										}
+print_number(num / 10);
+}
+_putchar((num % 10) + 48);
 }

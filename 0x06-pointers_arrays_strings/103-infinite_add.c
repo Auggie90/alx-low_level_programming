639c1eff4772beb20b7ae256a23fbb45d4a1ba63
“Auggie90” <auggieobimma@gmail.com>
2022-04-25 06:13:37 -0700

    ‘message’

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,79 +1,53 @@
 #include "main.h"
 
 /**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
- */
-
-void rev_string(char *n)
-{
-int i = 0;
-int j = 0;
-char temp;
-
-while (*(n + i) != '\0')
-{
-i++;
-}
-i--;
-
-for (j = 0; j < i; j++, i--)
-{
-temp = *(n + j);
-														*(n + j) = *(n + i);
-														*(n + i) = temp;
-														}
-}
-
-/**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
+ * infinite_add - add two numbers
+ * @n1: num1 param.
+ * @n2: num2 param.
+ * @r: buffer that the function will use to store the result
  * @size_r: buffer size
- * Return: pointer to calling function
+ * Return: char*
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int overflow = 0, i = 0, j = 0, digits = 0;
-int val1 = 0, val2 = 0, temp_tot = 0;
-
-while (*(n1 + i) != '\0')
+int i = 0, j = 0, calc, longest, n1int, n2int, sum = 0;
+while (n1[i] != '\0')
 i++;
-while (*(n2 + j) != '\0')
+while (n2[j] != '\0')
 j++;
-i--;
-j--;
-if (j >= size_r || i >= size_r)
+if (i >= j)
+longest = i;
+else
+longest = j;
+if ((longest + 1) >= size_r)
 return (0);
-while (j >= 0 || i >= 0 || overflow == 1)
+r[longest + 1] = '\0';
+i--, j--, size_r--;
+n1int = n1[i] - 48, n2int = n2[j] - 48;
+while (longest >= 0)
 {
-														if (i < 0)
-														val1 = 0;
-														else
-														val1 = *(n1 + i) - '0';
-														if (j < 0)
-													
-														val2 = 0;
-														else
-														val2 = *(n2 + j) - '0';
-														temp_tot = val1 + val2 + overflow;
-														if (temp_tot >= 10)
-																												overflow = 1;
-														else
-																												overflow = 0;
-																												if (digits >= (size_r - 1))
-																												return (0);
-																												*(r + digits) = (temp_tot % 10) + '0';
-																												digits++;
-																												j--;
-																												i--;
-																												}
-if (digits == size_r)
-return (0);
-*(r + digits) = '\0';
-rev_string(r);
+calc = n1int + n2int + sum;
+if (calc >= 10)
+sum = calc / 10;
+else
+sum = 0;
+if (calc > 0)
+r[longest] = (calc % 10) + 48;
+else
+r[longest] = '0';
+if (i > 0)
+i--, n1int = n1[i] - 48;
+else
+n1int = 0;
+if (j > 0)
+j--, n2int = n2[j] - 48;
+else
+n2int = 0;
+longest--, size_r--;
+}
+if (r[0] == '0')
+return (r + 1);
+else
 return (r);
 }

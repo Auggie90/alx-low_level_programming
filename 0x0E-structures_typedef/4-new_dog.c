b3f29a800ef975b6ca2918df087f0d323631da6c
“Auggie90” <auggieobimma@gmail.com>
2022-05-11 15:09:46 -0700

    ‘message’

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,10 +21,13 @@ if (p_dog == NULL || !(name) || !(owner))
 free(p_dog);
 return (NULL);
 }
+
 for (lname = 0; name[lname]; lname++)
 ;
+
 for (lowner = 0; owner[lowner]; lowner++)
 ;
+
 p_dog->name = malloc(lname + 1);
 p_dog->owner = malloc(lowner + 1);
 
@@ -35,6 +38,7 @@ if (!(p_dog->name) || !(p_dog->owner))
 														free(p_dog);
 														return (NULL);
 														}
+
 for (i = 0; i < lname; i++)
 														p_dog->name[i] = name[i];
 p_dog->name[i] = '\0';

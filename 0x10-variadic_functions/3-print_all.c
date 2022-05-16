d799068a9d1d01a40071b872075e8abc3d9e4629
“Auggie90” <auggieobimma@gmail.com>
2022-05-16 15:30:10 -0700

    ‘message’

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -17,35 +17,36 @@ va_start(valist, format);
 while (format && format[i])
 {
 j = 0;
-														while (t_arg[j])
-														{
+while (t_arg[j])
+{
 														if (format[i] == t_arg[j] && c)
 														{
-																												printf(", ");
+														printf(", ");
 																												break;
 																												} j++;
 														}
-														switch (format[i])
-														{
+switch (format[i])
+{
 														case 'c':
+													
 														printf("%c", va_arg(valist, int)), c = 1;
-																												break;
-																												case 'i':
-																												printf("%d", va_arg(valist, int)), c = 1;
+														break;
+														case 'i':
+														printf("%d", va_arg(valist, int)), c = 1;
 																												break;
 																												case 'f':
 																												printf("%f", va_arg(valist, double)), c = 1;
 																												break;
-																																										case 's':
-																																										str = va_arg(valist, char *), c = 1;
-																																										if (!str)
+																												case 's':
+																												str = va_arg(valist, char *), c = 1;
+																												if (!str)
 																																										{
-																																																								printf("(nil)");
-																																																								break;
+																																										printf("(nil)");
+																																										break;
 																																																								}
 																																										printf("%s", str);
 																																										break;
 																																										} i++;
-														}
+}
 printf("\n"), va_end(valist);
 }
